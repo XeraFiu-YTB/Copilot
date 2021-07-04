3433f0e96826fc1dbdf1e430890711d1899fb598
Nathan Bastard <nathan.bastardpfeg@gmail.com>
2021-07-04 11:24:09 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function getBestMemeAccount() {
+    const FoutuCode = {
+        "Instagram": "https://www.instagram.com/foutucode/",
+        "Facebook": "https://www.facebook.com/FoutuCode/",
+        "Twitter": "https://twitter.com/FoutuCode",
+        "Discord": "https://discord.gg/Vz8yvEQnvj"
+    }
+
+    return FoutuCode;
+}
